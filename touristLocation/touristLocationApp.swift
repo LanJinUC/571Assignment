9d00bb3922f50e43d4934c8238dbf1fbf1252741
Lan Jin <lan.jin@ucalgary.ca>
2020-11-15 00:04:36 -0700

    Initial Commit

---
+++ touristLocation/touristLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  touristLocationApp.swift
+//  touristLocation
+//
+//  Created by Lan Jin on 2020-11-15.
+//
+
+import SwiftUI
+
+@main
+struct touristLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
